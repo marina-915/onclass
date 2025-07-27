7ba464e065cf89e7d62510558dcf8d9266981de6
MARINA KOBAYASHI <maron91517@gmail.com>
2025-07-27 17:38:04 +0900

    7/27学習

---
+++ Javascript/section4/test.js
@@ -0,0 +1,41 @@
+var boolean = true;
+if(boolean){
+    console.log(boolean);
+}
+ 
+var num = 100
+if(num == 100){
+    console.log(num);
+    console.log(num == 100);
+}
+
+var text = "JavaScript"
+if(text == "JavaScript"){
+    console.log(text);
+}
+
+//課題
+//１. 変数にtrueを代⼊し、ifの条件がtrueの時にconsole.log();で
+// 変数の中⾝を表⽰させる処理を作ってください。
+
+var boolean = true;
+if(boolean){
+    console.log(boolean);
+}
+
+
+//２. 変数に100を代⼊し、ifの条件がtrueの時にconsole.log();で
+// 変数の中⾝を表⽰させる処理を作ってください。
+var num = 100
+if(num == 100){
+    console.log(num);
+}
+
+
+//３. 変数に"JavaScript"を代⼊し、ifの条件がtrueの時にconsole.log();で
+// 変数の中⾝を表⽰させる処理を作ってください。
+
+var text = "JavaScript"
+if(text = "JavaScript"){
+    console.log(text);
+}
